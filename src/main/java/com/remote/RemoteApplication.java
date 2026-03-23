f454f0d5fca913c705dabe8fa675919aa0be1fc1
puja <charchilaxmi@gmail.com>
2026-03-23 12:18:24 +0530

    First Commit

--- src/main/java/com/remote/RemoteApplication.java
+++ src/main/java/com/remote/RemoteApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RemoteApplication {
 
 	public static void main(String[] args) {
+		int x = 100;
 		SpringApplication.run(RemoteApplication.class, args);
 	}
 
